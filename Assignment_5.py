acff7aaf5f636b54caed7824859041e7aeeaa316
azerata <smedemarkhammer@gmail.com>
2021-12-07 13:11:18 +0100

    Update Assignment_5.py

--- Assignment_5.py
+++ Assignment_5.py
@@ -57,22 +57,54 @@ def bubber_sort(liste:Link)->Link:
             led = led.tail
     return liste
 
-test = Link(2, Link(3, Link(4, Link(1, Link(2, None)))))
+test = Link(3, Link(2, Link(4, Link(1, Link(2, None)))))
 repr(test)
 
 # %%
 repr(bubber_sort(test))
 # %%
-class Link(object):
+class DLink(object):
     def __init__(self, val, prev, next):
         self.val = val
         self.prev = prev
         self.next = next
+    
+    def __str__(self) -> str:
+        x = []
+        link = self
+        while link:
+            x.append(link.val)
+            link = link.next
+        return str(x)
 
 def to_list(itr):
     link = None
     for val in reversed(itr):
-        link = Link(val, None, link)
+        link = DLink(val, None, link)
         if link.next is not None:
             link.next.prev = link
     return link
+
+def swap_down(x:DLink):
+    # Move the value in x forward in the lislt
+    # until it finds a value smaller than itself
+    while x.prev and x.val <= x.prev.val:
+        x.val ,x.prev.val = x.prev.val, x.val
+        x = x.prev
+
+def insertion_sort(x):
+    link = x
+    while link is not None:
+        print(x)
+        swap_down(link)
+        link = link.next
+    return x
+
+#%%
+o = [3, 2, 4, 1, 2]
+test = to_list(o)
+print(test)
+# %%
+insertion_sort(test)
+print(test)
+# %%
